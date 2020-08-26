a4d68cb2221246b0ae7cbe421f83696c38800ba8
Sakib <35862475+Sakib0194@users.noreply.github.com>
2020-08-26 21:12:44 +0600

    Add files via upload

---
+++ time_checker.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import time, mysql.connector
+import grab_data_two, update_data
+
+conn = mysql.connector.connect(host='62.77.159.42',user='sakib3',database='bitbot',password='@&G6hdM@EZJKQu010au*jpIjs7EsB', autocommit=True)
+cur = conn.cursor()
+
+
+while True:
+    if conn.is_connected() == True:
+        pass
+    else:
+        conn = mysql.connector.connect(host='62.77.159.42',user='sakib3',database='bitbot',password='@&G6hdM@EZJKQu010au*jpIjs7EsB', autocommit=True)
+        cur = conn.cursor()
+    not_pending = grab_data_two.descri_false(cur)
+    if not_pending == []:
+        time.sleep(7200)
+    else:
+        for i in not_pending:
+            current_time = time.time()
+            next_pay = grab_data_two.next_payout(i, cur)
+            if next_pay >= current_time:
+                update_data.payout_true(i, cur)
+        time.sleep(7200)
