afe7bd12ca3884b444b6e3e7cdcbb146904efbf2
Sakib <35862475+Sakib0194@users.noreply.github.com>
2020-09-03 06:08:38 +0600

    Update time_checker.py

--- time_checker.py
+++ time_checker.py
@@ -5,21 +5,26 @@ import grab_data_two, update_data
 conn = mysql.connector.connect(host='62.77.159.42',user='sakib3',database='bitbot',password='@&G6hdM@EZJKQu010au*jpIjs7EsB', autocommit=True)
 cur = conn.cursor()
 
-
-while True:
-    if conn.is_connected() == True:
-        pass
-    else:
-        conn = mysql.connector.connect(host='62.77.159.42',user='sakib3',database='bitbot',password='@&G6hdM@EZJKQu010au*jpIjs7EsB', autocommit=True)
-        cur = conn.cursor()
-    not_pending = grab_data_two.descri_false(cur)
-    if not_pending == []:
-        time.sleep(4000)
-    else:
-        for i in not_pending:
-            current_time = time.time()
-            next_pay = grab_data_two.payout_next(i, cur)
-            if next_pay <= current_time:
-                print(f'{i} available for payout')
-                update_data.payout_true(i, cur)
-        time.sleep(4000)
+try:
+    while True:
+        if conn.is_connected() == True:
+            pass
+        else:
+            conn = mysql.connector.connect(host='62.77.159.42',user='sakib3',database='bitbot',password='@&G6hdM@EZJKQu010au*jpIjs7EsB', autocommit=True)
+            cur = conn.cursor()
+        not_pending = grab_data_two.descri_false(cur)
+        if not_pending == []:
+            time.sleep(4000)
+        else:
+            for i in not_pending:
+                print(i)
+                current_time = time.time()
+                next_pay = grab_data_two.payout_next(i, cur)
+                if next_pay <= current_time:
+                    print(f'{i} available for payout')
+                    update_data.payout_true(i, cur)
+            time.sleep(4000)
+except Exception as e:
+    print(e)
+    print('got an error')
+    pass
